6d7b37eb4da2040f9df60c3cb25b5d63957af508
tajul7472 <tajul7472@gmail.com>
2023-03-01 00:01:49 +0600

    reducer

---
+++ todo/src/Component/redux/todos/initialState.js
@@ -0,0 +1,13 @@
+export const initialState=[
+    {
+        id:1,
+        text:"Learn React js",
+        completed:true
+    },
+    {
+        id:2,
+        text:"Learn Redux",
+        completed:false,
+        color:"red"
+    }
+]
